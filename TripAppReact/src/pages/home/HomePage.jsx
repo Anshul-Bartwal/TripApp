c6f58ce5a4c8e9711a91056c9d64b648b4936b95
Anshul Bartwal <anshulbartwal720@gmail.com>
2025-11-16 16:30:24 +0530

    Fixed issues due to bundling

--- TripAppReact/src/pages/home/HomePage.jsx
+++ TripAppReact/src/pages/home/HomePage.jsx
@@ -1,4 +1,4 @@
-import'./homepage.css';
+import './homepage.css';
 import { useState } from 'react';
 import { TripSetup } from './TripSetup';
 export function HomePage() {
@@ -24,37 +24,8 @@ export function HomePage() {
                 <TripSetup />
             }
 
-            <div id="chat-container" className="hidden">
-                <div id="chat-header">
-                    <span>Travel Assistant</span>
-                    <button id="close-chat">✖</button>
-                </div>
-                <div id="chat-box"></div>
-                <div id="chat-input-area">
-                    <input type="text" id="user-input" placeholder="Ask about places..." />
-                    <button id="send-btn">Send</button>
-                </div>
-            </div>
 
-            <div id="place-modal" className="hidden">
-                <div className="modal-content">
-                    <div className="photo-side">
-                        <button id="prev-img" className="slide-btn">‹</button>
-                        <img id="modal-img" src="" alt="Tourist Place" />
-                        <button id="next-img" className="slide-btn">›</button>
-                    </div>
-
-                    <div className="info-side">
-                        <h2 id="modal-title"></h2>
-                        <p id="modal-desc"></p>
-
-                        <div id="modal-details"></div>
-
-                        <button id="chat-about-btn" className="main-btn">💬 Ask AI About This Place</button>
-                        <button id="close-modal" className="close-btn">✖ Close</button>
-                    </div>
-                </div>
-            </div>
+            
         </span>
     )
 }
