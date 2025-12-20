5ac7019cd67ab10eb776f556eaadc3043da4e187
Anshul Bartwal <anshulbartwal720@gmail.com>
2025-12-20 12:09:38 +0530

    Added Starting Menu basics

--- TripAppReact/src/pages/home/HomePage.jsx
+++ TripAppReact/src/pages/home/HomePage.jsx
@@ -3,13 +3,32 @@ import { useState } from 'react';
 import { TripSetup } from './TripSetup';
 export function HomePage() {
     const [ showing,setShowing ] =  useState("overlay");
+    const [ showMenu,setShowMenu ] =  useState(false);
     localStorage.clear();
     function handleStartPlanning(){
         setShowing("trip-setup");
     }
+
+    function handleStart(e){
+        setShowMenu(true);
+        e.target.style.display="none";
+    }
     return (
         <span className="hero-bg">
-            {showing==="overlay" &&
+            <div className="Start">
+                <button onClick={handleStart} className='press-to-start'>Press to Start</button>
+            </div>
+            {showMenu && 
+                <div className='menuOptions'>
+                    <button id="start-btn" className="main-btn" onClick={handleStartPlanning}>Start Journey </button>
+                    <button className='main-btn' onClick={() => setShowing("OurChoice")}>Our Choice</button>
+                    <button className='main-btn'>Map</button>
+                    <button className='main-btn'>About Us</button>
+                </div>
+            }
+            
+
+            {/* {showing==="overlay" &&
                     <div className="overlay" id="landing">
                         <div className="home-container">
                             <h1>Explore Uttarakhand</h1>
@@ -18,10 +37,13 @@ export function HomePage() {
                         </div>
                     </div>
             }
-            
+            */}
             {showing==="trip-setup" &&
                 <TripSetup />
             }
+ 
+
+
 
 
             
--- TripAppReact/src/pages/home/TripSetup.jsx
+++ TripAppReact/src/pages/home/TripSetup.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 // import { Link } from 'react-router-dom';
 import'./homepage.css';
+import './tripsetup.css';
 
 export function TripSetup(){
     const [ tripDetails,setTripDetails ] = useState({
